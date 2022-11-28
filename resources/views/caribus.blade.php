bfb932e439c969ef9a1508dbb780047b55ed14a3
akhipo14 <akhipo14@gmail.com>
2022-11-28 12:15:08 +0700

    update caribus

--- resources/views/caribus.blade.php
+++ resources/views/caribus.blade.php
@@ -20,12 +20,27 @@
     </div>
     <div class="nav">
       <ul>
-        <li> <a href="home.php"class="active">Home</a></li>
-        <li> <a href="#">Bis</a></li>
-        <li> <a href="#">Kontak</a></li>
-        <li> <a href="tentangkami.php">Tentang Kami</a></li>
-        <li> <a href="register.php" >Register</a></li>
-        <li> <a href="login.php">Login</a></li>
+        <li> <a href="{{ url('home') }}" class="{{ Request::is('home') ? 'active' : 'active' }}">Home</a></li>
+                <li> <a href="{{ url('bis') }}" class="{{ Request::is('bis') ? 'active' : '' }}">Bis</a></li>
+                <li> <a href="#">Kontak</a></li>
+                <li> <a href="{{ url('about') }}" class="{{ Request::is('about') ? 'active' : '' }}">Tentang Kami</a>
+                </li>
+                @auth
+                    <li>Hai, Rehan</li>
+                    {{-- @empty(auth()->user()->picture)
+                    <img src="{{asset('img/noprofile.png')}}" alt="{{auth()->user()->name}}" class="d-flex justify-content-center" style="width: 30px; height: 30px;">
+                    @else
+                    <img src="{{asset('img/profil/'.auth()->user()->picture)}}" alt="{{auth()->user()->name}}" class="d-flex justify-content-center" style="width: 30px; height: 30px;">
+                    @endempty --}}
+                    <form action="{{ url('/logout') }}" method="POST">
+                        @csrf
+                        <button>Logout</button>
+                    </form>
+                @else
+                    <li> <a href="{{ url('register') }}" class="{{ Request::is('register') ? 'active' : '' }}">Register</a>
+                    </li>
+                    <li> <a href="{{ url('login') }}" class="{{ Request::is('login') ? 'active' : '' }}">Login</a></li>
+                @endauth
       </ul>
     </div>
   </div>
